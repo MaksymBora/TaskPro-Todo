bef930fb1a2432496b7ef78b7674f45b47673438
MaksymBora <maxboraod@gmail.com>
2024-03-12 20:46:34 +0100

    add theme selector

---
+++ src/components/Header/DropdownMenu.tsx
@@ -0,0 +1,68 @@
+import { FC, useState } from 'react';
+import css from './ThemeSelect.module.css';
+
+interface ThemeButtonProps {
+  theme: string;
+  onClick: () => void;
+}
+
+const ThemeButton: FC<ThemeButtonProps> = ({ theme, onClick }) => {
+  return (
+    <button type="button" onClick={onClick} tabIndex={0}>
+      {theme}
+    </button>
+  );
+};
+
+export const DropdownMenu: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedTheme, setSelectedTheme] = useState<string | null>(null);
+
+  const handleTheme = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
+  const handleThemeSelect = (theme: string) => {
+    setSelectedTheme(theme);
+    setIsOpen(false);
+
+    console.log(selectedTheme);
+  };
+
+  // useEffect(() => {
+  //   console.log('selectedTheme changed:', selectedTheme);
+  // }, [selectedTheme]);
+
+  return (
+    <>
+      <div className={css.themeWrapper}>
+        <button type="button" className={css.themeButton} onClick={handleTheme}>
+          Theme
+          <svg fill="#161616" viewBox="0 0 32 32" width="16px" height="16px">
+            <path
+              stroke="currentColor"
+              d="M16.946 17.867c-0.124 0.125-0.271 0.224-0.434 0.292s-0.337 0.103-0.513 0.103c-0.176 0-0.35-0.035-0.513-0.103s-0.31-0.167-0.434-0.292l-6.107-6.12c-0.124-0.125-0.271-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292c-0.248 0.25-0.388 0.588-0.388 0.94s0.139 0.69 0.388 0.94l6.12 6.12c0.75 0.749 1.767 1.17 2.827 1.17s2.077-0.421 2.827-1.17l6.12-6.12c0.246-0.248 0.385-0.584 0.387-0.933 0.001-0.175-0.033-0.349-0.099-0.512s-0.164-0.31-0.288-0.435c-0.124-0.125-0.272-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292l-6.107 6.12z"
+            />
+          </svg>
+        </button>
+      </div>
+      {isOpen && (
+        <ul className={css.dropdownMenu}>
+          <li>
+            <ThemeButton
+              theme="Light"
+              onClick={() => handleThemeSelect('light')}
+            />
+          </li>
+          <li>
+            {' '}
+            <ThemeButton
+              theme="Dark"
+              onClick={() => handleThemeSelect('dark')}
+            />
+          </li>
+        </ul>
+      )}
+    </>
+  );
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import Icon from '../utils/Icon';
+import { DropdownMenu } from './DropdownMenu';
 import css from './Header.module.css';
 
 const Header = () => {
@@ -12,17 +13,39 @@ const Header = () => {
         </div>
 
         <nav className={css.nav}>
-          <form className={css.themeForm}>
+          {/* <form className={css.themeForm}>
             <label htmlFor="theme-select" className={css.selectLabel}>
               Theme select
             </label>
 
             <select name="theme" id="theme-select" className={css.themeSelect}>
-              <option selected>Theme</option>
-              <option value="light">Light</option>
+              <option value="light" defaultChecked>
+                Light
+              </option>
               <option value="dark">Dark</option>
             </select>
-          </form>
+          </form> */}
+
+          {/* <div className={css.themeWrapper}>
+            <button type="button" className={css.themeButton}>
+              Theme
+              <svg
+                fill="#161616"
+                viewBox="0 0 32 32"
+                width="16px"
+                height="16px"
+              >
+                <path
+                  stroke="currentColor"
+                  d="M16.946 17.867c-0.124 0.125-0.271 0.224-0.434 0.292s-0.337 0.103-0.513 0.103c-0.176 0-0.35-0.035-0.513-0.103s-0.31-0.167-0.434-0.292l-6.107-6.12c-0.124-0.125-0.271-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292c-0.248 0.25-0.388 0.588-0.388 0.94s0.139 0.69 0.388 0.94l6.12 6.12c0.75 0.749 1.767 1.17 2.827 1.17s2.077-0.421 2.827-1.17l6.12-6.12c0.246-0.248 0.385-0.584 0.387-0.933 0.001-0.175-0.033-0.349-0.099-0.512s-0.164-0.31-0.288-0.435c-0.124-0.125-0.272-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292l-6.107 6.12z"
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* Dropdown Menu */}
+
+          <DropdownMenu />
 
           <p className={css.userName}>Maks</p>
 
