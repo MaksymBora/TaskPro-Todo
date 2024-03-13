e3c310608289f0cdade317600d3762810816e91d
MaksymBora <maxboraod@gmail.com>
2024-03-13 19:30:38 +0100

    add theme switcher

--- src/components/Header/DropdownMenu.tsx
+++ src/components/Header/DropdownMenu.tsx
@@ -8,7 +8,12 @@ interface ThemeButtonProps {
 
 const ThemeButton: FC<ThemeButtonProps> = ({ theme, onClick }) => {
   return (
-    <button type="button" onClick={onClick} tabIndex={0}>
+    <button
+      type="button"
+      onClick={onClick}
+      tabIndex={0}
+      className={css.themeSelector}
+    >
       {theme}
     </button>
   );
@@ -34,18 +39,17 @@ export const DropdownMenu: FC = () => {
   // }, [selectedTheme]);
 
   return (
-    <>
-      <div className={css.themeWrapper}>
-        <button type="button" className={css.themeButton} onClick={handleTheme}>
-          Theme
-          <svg fill="#161616" viewBox="0 0 32 32" width="16px" height="16px">
-            <path
-              stroke="currentColor"
-              d="M16.946 17.867c-0.124 0.125-0.271 0.224-0.434 0.292s-0.337 0.103-0.513 0.103c-0.176 0-0.35-0.035-0.513-0.103s-0.31-0.167-0.434-0.292l-6.107-6.12c-0.124-0.125-0.271-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292c-0.248 0.25-0.388 0.588-0.388 0.94s0.139 0.69 0.388 0.94l6.12 6.12c0.75 0.749 1.767 1.17 2.827 1.17s2.077-0.421 2.827-1.17l6.12-6.12c0.246-0.248 0.385-0.584 0.387-0.933 0.001-0.175-0.033-0.349-0.099-0.512s-0.164-0.31-0.288-0.435c-0.124-0.125-0.272-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292l-6.107 6.12z"
-            />
-          </svg>
-        </button>
-      </div>
+    <div className={css.themeWrapper}>
+      <button type="button" className={css.themeButton} onClick={handleTheme}>
+        Theme
+        <svg fill="#161616" viewBox="0 0 32 32" width="16px" height="16px">
+          <path
+            stroke="currentColor"
+            d="M16.946 17.867c-0.124 0.125-0.271 0.224-0.434 0.292s-0.337 0.103-0.513 0.103c-0.176 0-0.35-0.035-0.513-0.103s-0.31-0.167-0.434-0.292l-6.107-6.12c-0.124-0.125-0.271-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292c-0.248 0.25-0.388 0.588-0.388 0.94s0.139 0.69 0.388 0.94l6.12 6.12c0.75 0.749 1.767 1.17 2.827 1.17s2.077-0.421 2.827-1.17l6.12-6.12c0.246-0.248 0.385-0.584 0.387-0.933 0.001-0.175-0.033-0.349-0.099-0.512s-0.164-0.31-0.288-0.435c-0.124-0.125-0.272-0.224-0.434-0.292s-0.337-0.103-0.513-0.103c-0.176 0-0.35 0.035-0.513 0.103s-0.31 0.167-0.434 0.292l-6.107 6.12z"
+          />
+        </svg>
+      </button>
+
       {isOpen && (
         <ul className={css.dropdownMenu}>
           <li>
@@ -63,6 +67,6 @@ export const DropdownMenu: FC = () => {
           </li>
         </ul>
       )}
-    </>
+    </div>
   );
 };
