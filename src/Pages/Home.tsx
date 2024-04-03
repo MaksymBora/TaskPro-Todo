2a16e895e1c7aff2a7089597a894bd1bf4396ac5
MaksymBora <maxboraod@gmail.com>
2024-04-04 00:08:36 +0200

    create btn to add new column

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import Filter from '@/components/Filter/Filter';
 import css from './Home.module.css';
+import Icon from '@/components/utils/Icon';
 
 function Home() {
   return (
@@ -9,10 +10,25 @@ function Home() {
         <Filter />
       </div>
 
+      <div>
+        <ul className={css.columnsList}>
+          <button type="button" className={css.createColumnBtn}>
+            <Icon
+              name="icon-plus"
+              width="14px"
+              height="14px"
+              fill="#ffffff"
+              className={css.addColumnIcon}
+            />{' '}
+            Add another column
+          </button>
+        </ul>
+      </div>
+
       {/* <div className={css.defaultBoard}>
         <p className={css.emptyBoardText}>
           Before starting your project, it is essential
-          <span className={css.boardCreation}> to create a board</span> to
+          <span className={css.boardCreationSpan}> to create a board</span> to
           visualize and track all the necessary tasks and milestones. This board
           serves as a powerful tool to organize the workflow and ensure
           effective collaboration among team members.
