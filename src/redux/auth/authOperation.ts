9508ae117246e6e5294aacf01a93af972b042953
MaksymBora <maxboraod@gmail.com>
2024-06-05 23:18:08 +0200

    update axios url

--- src/redux/auth/authOperation.ts
+++ src/redux/auth/authOperation.ts
@@ -1,7 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+// axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://task-todo-backend-awda.onrender.com/api';
 
 const setAuthHeader = (token: string) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
